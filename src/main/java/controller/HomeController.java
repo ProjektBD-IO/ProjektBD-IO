58189c2c6b96be2b7b5b40e71833b861b083e0f6
szymoxD <szymongmyz1@vp.pl>
2023-03-28 22:15:35 +0200

    basic stronav2

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody
     public String home(){
-        return "test";
+        return "index";
     }
 }
