64467706be9344940d48b39ce0078010e03475bc
karoljaroslawski <karoljaroslawski2001@o2.pl>
2023-03-28 18:27:19 +0200

    Created project

---
+++ src/main/java/com/app/changif/ChangifApplication.java
@@ -0,0 +1,14 @@
+package com.app.changif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangifApplication.class, args);
+
+	}
+	
+}
---
+++ src/test/java/com/app/changif/ChangifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.changif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
