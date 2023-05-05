bd7dcfb36fb333ea5187b8198cf66b58b2e7c51c
PiotrGrabias <129091921+PiotrGrabias@users.noreply.github.com>
2023-05-05 23:55:50 +0200

    Delete search.js

--- src/search.js
+++
@@ -1,9 +0,0 @@
-function SearchBar() {
-    return (
-      <form>
-        <input type="text" placeholder="Wyszukaj..." />
-        <button type="submit">Szukaj</button>
-      </form>
-    );
-  }
-  export default SearchBar;
