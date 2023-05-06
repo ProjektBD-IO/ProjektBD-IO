37034132f0b72117bb3ddb76ffddbacdd7ab6099
SpizganyTomek <oskarpiotrek80@gmail.com>
2023-05-06 08:34:42 +0200

    feat: checkPrivateAccess method in GifService

--- src/main/java/com/app/changif/gif/GifService.java
+++ src/main/java/com/app/changif/gif/GifService.java
@@ -1,8 +1,13 @@
 package com.app.changif.gif;
 
-import com.app.changif.category.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import io.vavr.control.Option;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.nio.file.AccessDeniedException;
+import java.util.Objects;
 
 @Service
 public class GifService {
@@ -10,6 +15,17 @@ public class GifService {
     @Autowired
     private GifRepository gifRepository;
 
-    // TODO: Implement service methods
+    private Gif checkPrivateAccess(Integer gifId) throws AccessDeniedException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentNickname = authentication.getName();
 
+        Gif gif = Option.ofOptional(gifRepository.findById(gifId))
+                .getOrElseThrow(() -> new RuntimeException("Gif not found"));
+        String gifOwnerNickname = gif.getCreator().getNickname();
+
+        if (Objects.equals(currentNickname, gifOwnerNickname)) {
+            return gif;
+        }
+        throw new AccessDeniedException("Dostep zablokowany: nie jestes wlascicielem gifa");
+    }
 }
