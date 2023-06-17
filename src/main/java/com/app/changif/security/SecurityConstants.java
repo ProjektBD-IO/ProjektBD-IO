dbfffbede7f46dc862d75abaa63f44ecf79dd933
szymoxD <szymongmyz1@vp.pl>
2023-06-17 19:40:31 +0200

    fixed token

--- src/main/java/com/app/changif/security/SecurityConstants.java
+++ src/main/java/com/app/changif/security/SecurityConstants.java
@@ -3,7 +3,7 @@ package com.app.changif.security;
 public class SecurityConstants {
 
     public static final String SECRET = "SECRET_KEY";
-    public static final long EXPIRATION_TIME = 900_000; // 15 mins
+    public static final long EXPIRATION_TIME = 1800_000; // 15 mins
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/login";
